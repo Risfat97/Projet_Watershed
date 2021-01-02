2e273b709c8b0cfc85d3091fa879a0511fc219ca
Tafsir M N <tamsir97@gmail.com>
2021-01-02 14:53:21 +0100

    Modification de quelques types en utilisant les types de la bibliothèque stdint.h

--- imagepng.h
+++ imagepng.h
@@ -6,23 +6,18 @@
 
 //Définition de la structure permettant de stocker les données relatives à l'image en noir et blanc
 typedef struct{
-    unsigned hauteur;   // hauteur de l'image
-    unsigned largeur;   // largeur de l'image
-    unsigned char** rouge;  // canal rouge de l'image
+    uint64_t hauteur;   // hauteur de l'image
+    uint64_t largeur;   // largeur de l'image
+    uint8_t **rouge;  // canal rouge de l'image
 } imagepng;
 
-/**
- * Initialise et retourne une imagepng vide avec une hauteur et une largeur à 0,
- * et le tableau 2d à NULL.
- */
-imagepng initialise();
 
 /**
  * Initialise une imagepng avec la hauteur et la largeur spécifiées,
  * le tableau 2d de dimension hauteur x largeur remplis de 0 puis 
  * retourne cette image.
  */
-imagepng allouer_image(unsigned hauteur, unsigned largeur);
+imagepng allouer_image(uint64_t hauteur, uint64_t largeur);
 
 /**
  * Lit une image PNG dont le nom est le nom spécifié, la stocke dans une structure imagepng
